e332a2263e10f6b1aa91ea76d5664b5df2d4414e
dylancrter <dylanc.rter@gmail.com>
2025-02-20 16:29:32 -0500

    added auth0 jwt for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,32 @@
 import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import { auth } from 'express-oauth2-jwt-bearer';
 import userRoutes from './routes/userRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
+const jwtCheck = auth({
+  audience: 'https://predictyourcrops.com',
+  issuerBaseURL: 'https://dev-70z0yvfe2suqrf7k.us.auth0.com/',
+  tokenSigningAlg: 'RS256',
+});
 
 // middleware
 app.use(express.json());
+app.use(jwtCheck);
+app.use(cors());
 app.use((req: Request, res: Response, next: NextFunction) => {
   console.log(`URL: ${req.originalUrl}, Datetime: ${new Date()}`);
   next();
 });
 
-// routes
+// public routes
 app.use('/api/users', userRoutes);
 
+// protected routes
+
+// listen
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
